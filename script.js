1d4595b4a4742170623fd87be577020262750186
B3lk00 <belmins1617@gmail.com>
2026-01-18 02:14:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-
 function updateClock() {
   const clock = document.getElementById("digitalClock");
+  const dateEl = document.getElementById("digitalDate");
   const now = new Date();
 
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const h = String(now.getHours()).padStart(2, "0");
+  const m = String(now.getMinutes()).padStart(2, "0");
+  const s = String(now.getSeconds()).padStart(2, "0");
 
-  clock.textContent = `${hours}:${minutes}:${seconds}`;
+  clock.innerHTML = `${h}<span class="blink">:</span>${m}<span class="blink">:</span>${s}`;
+
+  const day = String(now.getDate()).padStart(2, "0");
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const year = now.getFullYear();
+
+  dateEl.textContent = `${day}.${month}.${year}`;
 }
 
 setInterval(updateClock, 1000);
