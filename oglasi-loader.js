3d5961d674c8eacf0370ce6c3664fa2453f5461a
B3lk00 <belmins1617@gmail.com>
2026-01-19 00:31:52 +0100

    Update oglasi-loader.js

--- oglasi-loader.js
+++ oglasi-loader.js
@@ -1,1 +1,6 @@
+async function ucitajListuOglasa() {
+  // Netlify build će poslužiti ove fajlove statično; najčistije je imati i index fajl,
+  // ali za start možeš ručno (ili ja ti dam varijantu koja builda index).
+  throw new Error("Treba index oglasa (oglasi/index.json) ili build korak.");
+}
 
